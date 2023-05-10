bd89dc35c89be0f5a663adbbb8b2cf78105f2b9a
OhHalfmoon <sangoh7395@gmail.com>
2023-05-10 15:05:48 +0900

    스타일  마무리

---
+++ src/CSSModule.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './CSSModule.module.scss';
+import classNames from 'classnames/bind';
+
+const CSSModule = () => {
+  // active
+  // disabled, selected, cheked
+  const cx = classNames.bind(styles);
+  return (
+    <div className={cx("wrapper", "inverted")}>
+      안녕하세요, 저는 <span className='something'>CSSModule!</span>
+    </div>
+  );
+};
+
+export default CSSModule;
